7c978e61ce916e861effa7618071bf319af3645f
GEVIC21 <ndilbedouvic@gmail.com>
2024-10-29 21:35:28 +0000

    first commit

---
+++ admin/includes/styles.php
@@ -0,0 +1,19 @@
+
+
+
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon.ico">
+<!-- inject:css-->
+<link rel="stylesheet" href="assets/vendor_assets/css/apexcharts.min.css">
+<link rel="stylesheet" href="assets/vendor_assets/css/datepicker.min.css">
+<link rel="stylesheet" href="assets/vendor_assets/css/line-awesome.min.css">
+<link rel="stylesheet" href="assets/vendor_assets/css/nouislider.min.css">
+<link rel="stylesheet" href="assets/vendor_assets/css/quill.snow.css">
+<link rel="stylesheet" href="assets/vendor_assets/css/svgMap.min.css">
+<!-- styles.php inclus -->
+<link rel="stylesheet" href="tailwind.css">
+<!-- endinject -->
+
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
+<!-- Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@4.0.8/css/line.min.css">
